e0457588359b2d76888f210b7a6fd69aaabe3079
Matt1218 <welfleym@gmail.com>
2022-01-19 21:31:18 -0500

    added activation button for autoclicker

--- donut-maker-app.js
+++ donut-maker-app.js
@@ -3,25 +3,24 @@ import DonutMaker from './donut-maker';
 const myDonutsDiv = document.querySelector('#myDonuts');
 const myAutoClicksDiv = document.querySelector('#myAutoClicks');
 
-let currentPrice = 100;
+let currentPrice = 10;
 let ticks = 0;
 
-
 const createdDonut = new DonutMaker(0, 0, 0);
-
-
 const createDonutBtn = document.querySelector('#donut-button');
 const createdDonutSection = document.createElement('section');
 const donutClicksPara = document.createElement('p');
 const buyAutoClickerBtn = document.querySelector('#autoclicker-button');
 const createdAutoClickSection = document.createElement('section');
 const autoClicksPara = document.createElement('p');
+const actAutoClickerBtn = document.querySelector('#actautoclicker-button');
 
 renderPage();
 
 function renderPage() {
     addADonut();
     buyAutoClicker();
+    actAutoClicker();
 }
 
 function addADonut() {
@@ -34,10 +33,8 @@ function addADonut() {
 
         createdDonut.donutClick();
 
-
         donutClicksPara.innerText = 'You have made ' + Math.floor(createdDonut.donutCount) + ' donuts!';
     });
-
 
 }
 
@@ -48,7 +45,6 @@ function buyAutoClicker() {
     createdDonutSection.appendChild(donutClicksPara);
     myDonutsDiv.appendChild(createdDonutSection);
 
-
     buyAutoClickerBtn.addEventListener('click', () => {
         if (currentPrice <= createdDonut.donutCount) {
             createdDonut.donutCount = createdDonut.donutCount - currentPrice;
@@ -56,27 +52,27 @@ function buyAutoClicker() {
             createdDonut.autoClickerCount += 1;
         }
 
+        autoClicksPara.innerText = 'You have purchased ' + createdDonut.autoClickerCount + ' auto click(s)';
+        // console.log(createdDonut.donutCount);
+        // console.log(currentPrice);
 
+    });
+
+}
+
+function actAutoClicker() {
+    actAutoClickerBtn.addEventListener('click', () => {
         if (createdDonut.autoClickerCount >= 1) {
             let autoClick = setInterval(() => {
                 // console.log(ticks ++, createdDonut.donutCount++);
                 ticks++;
                 createdDonut.donutCount += 1;
-                donutClicksPara.innerText = 'Automatically making: ' + Math.floor(createdDonut.donutCount) + ' donuts';
+                donutClicksPara.innerText = 'Total amount of donuts: ' + Math.floor(createdDonut.donutCount);
                 if (ticks >= 100000) {
                     clearInterval(autoClick);
                     // console.log('stop');
                 }
             }, 1000);
         }
-
-
-        autoClicksPara.innerText = 'You have purchased ' + createdDonut.autoClickerCount + ' auto click(s)';
-        console.log(createdDonut.donutCount);
-        console.log(currentPrice);
-
     });
-
-
-
 }
